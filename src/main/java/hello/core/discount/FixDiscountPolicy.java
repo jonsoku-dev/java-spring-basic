b68ae1f4efe04aff206085ef37e909b32525dc82
jongseok-lee <jongseok.lee@linecorp.com>
2020-12-04 00:37:57 +0900

    조회된 빈이 2개 이상일때 (테스트에서에러가남)

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
