a37bbe55a6f886fd6fc5a00c82b1c34d1c0168cd
jongseok-lee <jongseok.lee@linecorp.com>
2020-12-04 00:33:22 +0900

    롬복과 최신트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,18 +7,20 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor // final이 붙어있는걸 가지고 생성자를 만들어준다.
 public class OrderServiceImpl implements OrderService {
     // final이면 반드시 생성자를 이용하여 할당해야한다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+////    @Autowired  // 생성자의 autowired는 생략가능하다.
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
