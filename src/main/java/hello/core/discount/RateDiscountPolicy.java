48e86e62a89d17f237d7678223ae1bd44322b0ec
jongseok-lee <jongseok.lee@linecorp.com>
2020-12-04 00:47:10 +0900

    @Autowired, @Qualifier, @Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,11 +1,14 @@
 package hello.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
 @Component
+@Primary // 똑같은 빈이 존재할때, 이놈이 첫번째 우선권을 가진다.
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
